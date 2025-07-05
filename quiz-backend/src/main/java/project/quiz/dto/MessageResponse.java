b12000d28dff5cb0d8733df8722657b3e7fd8c24
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-07-05 22:05:20 +0530

    create dtos

---
+++ quiz-backend/src/main/java/project/quiz/dto/MessageResponse.java
@@ -0,0 +1,12 @@
+package project.quiz.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageResponse {
+    private String message;
+}
+
