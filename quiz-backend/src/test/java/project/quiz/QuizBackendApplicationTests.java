bacc6d97964f9432ae2aeacf2d31549c408d91e2
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-07-05 13:22:59 +0530

    Initialized projects

---
+++ quiz-backend/src/test/java/project/quiz/QuizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package project.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
