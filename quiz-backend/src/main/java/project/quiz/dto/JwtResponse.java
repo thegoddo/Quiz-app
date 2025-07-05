9ce67d280a39d06687523801f6a712c7653cf19e
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-07-05 22:04:24 +0530

    set up jwt

---
+++ quiz-backend/src/main/java/project/quiz/dto/JwtResponse.java
@@ -0,0 +1,25 @@
+package project.quiz.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private List<String> roles;
+
+    public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
+        this.token = accessToken;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+}
