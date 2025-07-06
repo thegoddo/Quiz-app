906da27950e5e292b39829d12e0698da72dbd357
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-07-07 00:29:11 +0530

    complete index page

--- quiz-frontend/src/App.jsx
+++ quiz-frontend/src/App.jsx
@@ -1,11 +1,34 @@
 import "./App.css";
-import Login from "./component/Login";
-import Register from "./component/Register";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigator = useNavigate();
+  const handleClick = () => {
+    // Add your logic here, e.g., start the quiz or navigate
+    navigator("/quiz");
+  };
+
   return (
     <>
       <div>
+        <div
+          className="d-flex align-items-center justify-content-center"
+          style={{ height: "200px" }}
+        >
+          <h1 className="text-center">Welcome!!! </h1>
+        </div>
+        <h2>
+          Click the "Start Now" button to start playing😁
+          <br />
+          <br />
+          <button
+            onClick={handleClick}
+            className="btn btn-primary"
+            style={{ fontSize: "24px" }}
+          >
+            Start Now
+          </button>
+        </h2>
       </div>
     </>
   );
