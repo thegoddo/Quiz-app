87ef65cb69bc0197e3cbb2c7c25ccd8994f70b6b
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-07-06 23:18:54 +0530

    suppressed warnings

--- quiz-backend/src/main/java/project/quiz/security/jwt/AuthTokenFilter.java
+++ quiz-backend/src/main/java/project/quiz/security/jwt/AuthTokenFilter.java
@@ -2,13 +2,11 @@ package project.quiz.security.jwt;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -31,6 +29,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
         this.userDetailsService = userDetailsService;
     }
 
+    @SuppressWarnings("null")
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
